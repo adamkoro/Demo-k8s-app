563c9c1632d3a310b6ab69aeadca6128b6db09e1
adamkoro <adam.koronics@gmail.com>
2023-01-04 23:04:36 +0000

    Message handler implemented

---
+++ backend/mq/producer/app/messageHandler/message.go
@@ -0,0 +1,16 @@
+package messageHandler
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	WarningLogger = log.New(os.Stdout, "[WARNING] ", log.Ldate|log.Ltime)
+	ErrorLogger   = log.New(os.Stderr, "[ERROR] ", log.Ldate|log.Ltime)
+	InfoLogger    = log.New(os.Stdout, "[INFO] ", log.Ldate|log.Ltime)
+)
+
+func IsError(err error) bool {
+	return err != nil
+}
