d336c39ccc4706c4a9fdae097f6a75557307ccf2
adamkoro <adam.koronics@gmail.com>
2023-01-05 20:30:58 +0000

    Init section added

--- backend/mq/producer/app/main.go
+++ backend/mq/producer/app/main.go
@@ -5,22 +5,33 @@ import (
 	"net/http"
 
 	"demo-k8s-app/mq-communicator/env"
+	"demo-k8s-app/mq-communicator/messageHandler"
+	"demo-k8s-app/mq-communicator/mq"
 	endpoints "demo-k8s-app/mq-communicator/v1"
 
 	"github.com/gin-gonic/gin"
 )
 
-// @title MQ-Producer Swagger
-// @version 1.0
-// @description RabbitMQ Producer(sender) API
-func main() {
+func init() {
 	env.CheckEnvs()
+
+	var err error
+	endpoints.Connection, err = mq.ConnectToMq(mq.CreateConnUrl(env.Username, env.Password, env.MqHost, env.Port, env.Vhost))
+	errorExist := messageHandler.IsError(err)
+	if errorExist {
+		msg := "Failed to connect to RabbitMQ"
+		messageHandler.ErrorLogger.Printf("%s: %s", err, msg)
+	}
+}
+
+func main() {
 	router := gin.Default()
 
 	// Api V1
 	v1 := router.Group("/v1")
 	{
 		v1.GET("/ping", endpoints.Ping)
+		v1.GET("/health", endpoints.Health)
 		v1.GET("/push", endpoints.SendMessageToMq)
 	}
 
