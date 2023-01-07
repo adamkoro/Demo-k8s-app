0de2dfff7cf803e0dae65135d43e23f7aa58d96e
adamkoro <adam.koronics@gmail.com>
2023-01-07 10:59:27 +0000

    Swagger implemented

--- backend/mq/producer/app/main.go
+++ backend/mq/producer/app/main.go
@@ -10,8 +10,14 @@ import (
 	"demo-k8s-app/mq-communicator/mq"
 	endpoints "demo-k8s-app/mq-communicator/v1"
 
+	docs "demo-k8s-app/mq-communicator/docs"
+
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+var serviceEndpoint = "/producer/v1"
 
 func init() {
 	env.CheckEnvs()
@@ -33,16 +39,21 @@ func main() {
 		c.Redirect(http.StatusMovedPermanently, "/producer")
 	})
 	// Api V1
-	v1 := router.Group("/:servicename/v1")
+	docs.SwaggerInfo.BasePath = serviceEndpoint
+	v1 := router.Group(serviceEndpoint)
 	{
 		v1.GET("/ping", endpoints.Ping)
 		v1.GET("/health", endpoints.Health)
 		v1.GET("/push", endpoints.SendMessageToMq)
+		v1.StaticFile("/docs/swagger.json", "docs/swagger.json")
 	}
+
+	url := ginSwagger.URL("http://localhost:8081" + serviceEndpoint + "/docs/swagger.json") // The url pointing to API definition
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 
 	// Http server config
 	srv := &http.Server{
-		Addr:           ":8080",
+		Addr:           ":8081",
 		Handler:        router,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
